05411d5fd77348db7ab4a31e07df04d566520a9b
max <max@max-Latitude-5580>
2019-01-08 14:11:05 +0100

    ADD stuff

--- TP_RSF/src/Liens.java
+++ TP_RSF/src/Liens.java
@@ -1,6 +1,10 @@
 
 public class Liens {
 
+	protected int LIEN1 = 25;// débit 25MB/s
+	protected int LIEN2 = 50;
+	protected int LIEN3 = 100;
+	
 	public Liens() {
 		// TODO Auto-generated constructor stub
 	}
--- TP_RSF/src/Utilisateur.java
+++ TP_RSF/src/Utilisateur.java
@@ -1,6 +1,9 @@
 
 public class Utilisateur {
 
+	protected int TAILLEPACKET = 0;
+	
+	
 	public Utilisateur() {
 		// TODO Auto-generated constructor stub
 	}
