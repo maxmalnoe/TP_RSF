bf7ce8f0f875e2d6c9cb0d9148e5b5135976385a
max <max@max-Latitude-5580>
2019-01-08 15:56:45 +0100

    upadte2.0

--- TP_RSF/src/Paquet.java
+++ TP_RSF/src/Paquet.java
@@ -1,26 +1,27 @@
 
 public class Paquet {
+	 private static final int TAILLEPACKET = 0;
 	//paquet qui transite de la source à la destination, avec contraintes et taille différentes, délai à respecter pour chaque paquet
 	//paquet issu d'un flux particulier (audio, videao, stream)	
-	static int taillepacket;
-	static int delay;
-	static int data [];
+	 protected int TAILLEPACKET23 = 100;
+	 protected int delay;
+	 protected int data [];
 	
 	public Paquet() {
 		// TODO Auto-generated constructor stub
-		taillepacket = 100;
-		delay=0;
-		data = new int[taillepacket];
+		int taillepacket = TAILLEPACKET;
+		int delay=0;
+		int [] data = new int[TAILLEPACKET];
 		
 	}
 	
 	public static Paquet createPacket(String fluxname) {
 		Paquet p = new Paquet();
-		p.taillepacket = taillepacket;
+		p.taillepacket = TAILLEPACKET;
 		switch(fluxname) {
 			case ("flux1") :
 				p.delay=25;
-				for(int i=0; i<p.taillepacket;i++) {
+				for(int i=0; i<p.TAILLEPACKET;i++) {
 					if(Math.random()< 0.5) {
 						p.data[i]=0;
 					}
