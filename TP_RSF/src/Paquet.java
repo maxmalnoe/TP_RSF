875a3648755ca7c962673eff7968a079cd15396e
max <max@max-Latitude-5580>
2019-01-08 15:32:32 +0100

    paquet modif

--- TP_RSF/src/Paquet.java
+++ TP_RSF/src/Paquet.java
@@ -2,8 +2,31 @@
 public class Paquet {
 	//paquet qui transite de la source à la destination, avec contraintes et taille différentes, délai à respecter pour chaque paquet
 	//paquet issu d'un flux particulier (audio, videao, stream)	
+	protected int TAILLEPACKET = 100;
+	protected int DELAY = 0;
+	protected int DATA [];
+	
 	public Paquet() {
 		// TODO Auto-generated constructor stub
+		
+		
+	}
+	
+	public int [] createPacket(String fluxname) {
+		int tab []= new int [TAILLEPACKET];
+		switch(fluxname) {
+			case ("flux1") :
+				DELAY=25;
+				for(int i=0; i<TAILLEPACKET;i++) {
+					if(Math.random()< 0.5) {
+						tab[i]=0;
+					}
+					else {
+						tab[i]=1;
+					}
+				}
+		}
+		return tab;
 	}
 
 }
