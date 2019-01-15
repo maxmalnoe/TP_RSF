2e57bdbe98df6cc11e7e4d2ae4403961b9134a3b
max <max@max-Latitude-5580>
2019-01-15 10:54:51 +0100

    test

--- TP_RSF/src/Source.java
+++ TP_RSF/src/Source.java
@@ -4,27 +4,24 @@ import java.util.concurrent.ArrayBlockingQueue;
 public class Source {
 	//recout des flux que la source achemine sur la topologie jusqu'a la destination
 	protected static Queue<Paquet> listAttente;
+	protected static int id;
 	
 	public Source() {
 		// TODO Auto-generated constructor stub
+
 		
 	}
 	
 	public static void fillListAttente() {
-		listAttente = new ArrayBlockingQueue<Paquet>(20);
-		int i=0;
-		while(listAttente.size()<20) {
-			
 			Paquet p = new Paquet();
 			p = Paquet.createPacket("flux1");
-			p.id=i+1;
+			p.id=id+1;
 			p.createTime=System.currentTimeMillis();
 			try {
 			listAttente.add(p);
 			}catch (NullPointerException e) {
 				System.out.print("NullPointerException caught");
 			}
-		}
 		System.out.println(listAttente);
 		
 	}
@@ -32,18 +29,25 @@ public class Source {
 	public static void consumeListAttente() {
 		if(!listAttente.isEmpty()) {
 			
-		for (int i =0;i<20;i++) {
+		
 		Paquet p = listAttente.poll();
 		System.out.println("ID: "+p.id+", Date de création du paquet: "+p.createTime+"; Taille du paquet: "+p.taillepacket);
-		}
+		Paquet p1 = listAttente.poll();
+		System.out.println("ID: "+p1.id+", Date de création du paquet: "+p1.createTime+"; Taille du paquet: "+p1.taillepacket);
 		}
 		
 	}
 	
 	public static void main(String[] args) {
-		
-		fillListAttente();
-		consumeListAttente();
+		listAttente = new ArrayBlockingQueue<Paquet>(20);
+		id=0;
+		while(listAttente.size()<20) {
+			fillListAttente();
+			if(!listAttente.isEmpty()) {
+				consumeListAttente();
+			}
+		}
+
 	}
 
 }
