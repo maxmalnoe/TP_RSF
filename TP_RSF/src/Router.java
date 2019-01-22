7f8300c0ec65cf94dfae2cf08178b3c4cf32635f
fangkruger <fangkruger@FangKruger.home>
2019-01-22 12:14:28 +0100

    esapce

--- TP_RSF/src/Router.java
+++ TP_RSF/src/Router.java
@@ -26,7 +26,7 @@ public class Router {
 				da=System.currentTimeMillis();
 				System.out.println("ID: "+p.id+", Date de création du paquet: "+p.createTime+", Date arrivée du paquet: " +da);
 				boolean r = (da-dd)<p.delay;
-				System.out.println("Paquet arrivé dans les delai ? : "+ r+" ,contrainte du paquet"+p.delay+", delai réel: "+(da-dd)+", lien associé: "+Liens.cheminPaquet(p.delay).name);
+				System.out.println("Paquet arrivé dans les delai ? : "+ r+" ,contrainte du paquet "+p.delay+", delai réel: "+(da-dd)+", lien associé: "+Liens.cheminPaquet(p.delay).name);
 				lr.add(p);
 			//}
 			
