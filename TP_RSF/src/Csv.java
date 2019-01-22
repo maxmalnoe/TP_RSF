ce1a67faa45c54127e881ba931929af72833bac2
fangkruger <fangkruger@FangKruger.home>
2019-01-22 11:03:32 +0100

    fichier CSV OK plus qu'a le remplir avec ce que l'on souhaite

--- TP_RSF/src/Csv.java
+++ TP_RSF/src/Csv.java
@@ -2,20 +2,27 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Csv {
-	
-	
+
+
 	public static FileWriter remplissagecsv() throws IOException {
 		FileWriter fileWriter = new FileWriter("Resultats.csv");
-		fileWriter.append("blabla");
+		for(int i=0; i<10;i++) {
+			fileWriter.write("blabla");
+			for(int j=0; j<10;j++) {
+				fileWriter.write(',');
+				fileWriter.write("test" + j);
+			}
+			fileWriter.write('\n');
+		}
 		fileWriter.close();
 		return fileWriter;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		FileWriter fileWriter = remplissagecsv();
 		System.out.println(fileWriter);
-		
-		
+
+
 	}
 
 }
