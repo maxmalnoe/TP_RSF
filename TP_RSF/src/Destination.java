f95ebec056de31f85333a4ed03c1d31d2b54be83
max <max@max-Latitude-5580>
2019-01-22 10:33:56 +0100

    fd

--- TP_RSF/src/Destination.java
+++ TP_RSF/src/Destination.java
@@ -16,7 +16,7 @@ public class Destination {
 				da=System.currentTimeMillis();
 				System.out.println("ID: "+p.id+", Date de création du paquet: "+p.createTime+", Date arrivée du paquet: " +da);
 				boolean r = (da-dd)<p.delay;
-				System.out.println("Paquet arrivé dans les delai ? : "+ r+" ,contrainte du paquet "+p.delay+", delai réel: "+(da-dd)+", lien associé: "+Liens.cheminPaquet(p.delay).name);
+				System.out.println("Paquet arrivé dans les delai ? : "+ r+" ,contrainte du paquet"+p.delay+", delai réel: "+(da-dd)+", lien associé: "+Liens.cheminPaquet(p.delay).name);
 			}
 			
 		}
